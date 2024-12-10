4658850e07dadfc592fc6b1a0089421a97a07a9c
betancito <jeronimobeta90@gmail.com>
2024-12-10 00:41:56 -0500

    feat: country seeder ready using JSON file

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,31 +2,42 @@
 
 namespace Database\Seeders;
 use App\Models\Country;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class CountrySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
+        //Add countries JSON file path
+        $countriesPath = database_path('data\countries.json');
 
-        $response = Http::get('https://restcountries.com/v3.1/all');
-
-        if ($response -> sucessfull())
-        {
-            $countries = $response->json();
-
-            $countriesData = array_map(function ($country) {
-                return ['name' => $country['name']['common']];
-            }, $countries);
-
-            DB::table('countries')->insert($countriesData);
-        }else{
-            echo ('failed to fetch countries');
+        //Check path existence
+        if (!File::exists($countriesPath)) {
+            $this->command->error("The JSON countries file does not exist please add one to the data folder.");
+            return;
         }
+
+        //Decode the JSON file
+        $countries = json_decode(File::get($countriesPath), true);
+
+        //Check JSON file structure
+        if (!$countries) {
+            $this->command->error("The JSON file invalid, please check the sructure.");
+            return;
+        }
+
+        //Cycle to seed database with JSON file info
+        foreach ($countries as $country) {
+            Country::create([
+                'name' => $country['name'],
+            ]);
+        }
+
+        //Message to advise that seeding process was sucessfull
+        $this->command->info("Countries seeded successfully!");
     }
 }
