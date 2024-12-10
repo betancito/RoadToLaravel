1a04a6984ab2f72a6834d064d29a0fda03fd7fab
betancito <jeronimobeta90@gmail.com>
2024-12-10 00:14:54 -0500

    fix: removed guzzlehttp

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 use App\Models\Country;
 use Illuminate\Support\Facades\Http;
-//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class CountrySeeder extends Seeder
@@ -14,23 +14,17 @@ class CountrySeeder extends Seeder
     public function run(): void
     {
 
-
-
-        $response = Http::timeout(30)  // 30 seconds timeout
-    ->retry(3, 100)  // Retry 3 times with 100ms delay
-    ->withoutVerifying()  // Disable SSL verification for testing
-    ->get('https://restcountries.com/v3.1/all');
+        $response = Http::get('https://restcountries.com/v3.1/all');
 
         if ($response -> sucessfull())
         {
             $countries = $response->json();
 
-            foreach($countries as $country)
-            {
-                Country::create([
-                    'name' => $country['name'],
-                ]);
-            }
+            $countriesData = array_map(function ($country) {
+                return ['name' => $country['name']['common']];
+            }, $countries);
+
+            DB::table('countries')->insert($countriesData);
         }else{
             echo ('failed to fetch countries');
         }
